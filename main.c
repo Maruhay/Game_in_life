1f843b3891a2db2257abf170e4c3509cb49d6490
Maruhay <denismarush@gmail.com>
2019-02-22 20:59:17 +0100

    First commit

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 
 int main(){
-        for(int i = 0; i< 10;i++){
+        for(int i = 0; i < 10;i++){
                 for(int j = 0; j < 10; j++)
                         if (j == 7 && i == 4) printf("\u25d9 ");
                                 else printf("\u25cb ");
