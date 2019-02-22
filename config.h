acce89259040417bce0782c110aba641cf86e204
Waving666 <waving6wa@gmail.com>
2019-02-23 00:04:50 +0100

    Defalut Configuration + Configuration Structure

--- config.h
+++ config.h
@@ -1,3 +1,19 @@
 //Структура конфигурации: размер сетки, кол-во итераций, тип соседства, тип вывода
 //Чтение из файла конфигураций
 //Ф-я установки конфигураций по-умолчанию
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#include "stdbool.h"
+typedef struct Config{
+    int col;
+    int row;
+    int n_it;
+    char rule;
+    bool out;
+} *config;
+
+void defaultConfig(config conf);
+
+
+#endif
