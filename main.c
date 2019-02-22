2962c0c3392b07c807c8b696c63c4cf743b75219
Maruhay <denismarush@gmail.com>
2019-02-22 21:05:09 +0100

    Test commit

--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 int main(){
         for(int i = 0; i < 10;i++){
                 for(int j = 0; j < 10; j++)
-                        if (j == 7 && i == 4) printf("\u25d9 ");
+                        if (j == 7 && i == 4)
+                                printf("\u25d9 ");
                                 else printf("\u25cb ");
         printf("\n");
         }
