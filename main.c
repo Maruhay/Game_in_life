0f9891776845a733f93205c51e22ef286e0ced47
Waving666 <waving6wa@gmail.com>
2019-02-25 22:39:40 +0100

    v1.0

--- main.c
+++ main.c
@@ -1,23 +1,12 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "game.h"
-
-//Живая - \u25d9
-//Мёртвая - \u25cb
+#include "config.h"
 
 int main(int argc, char* argv[]){
         config conf = malloc(sizeof conf);
-        userConfig(conf);
-        bool **matrix = (bool **)malloc(conf->row * sizeof(bool *));
-        for (int i = 1 ; i <= conf->row; i++)
-                matrix[i] = (bool *)malloc(conf->col * sizeof(bool));
-        matGen(matrix, conf);
-        Neumann(matrix, conf);
-        for (int i = 1 ; i <= conf->row; i++) {
-                for (int j = 1; j <= conf->col; j++)
-                        printf("%d ", matrix[i][j]);
-                printf("\n");
-    }
+        printf("\e[1;1H\e[2J");
+        if(argc < 2)
+                chooseConfig(conf);
+        else ;
+        //Иначе чтение сейва и снова надо спросить про конфигурацию
+        startGame(conf, argv[1]);
         return 0;
 }
